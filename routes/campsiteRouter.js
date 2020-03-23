769333e09cc22ff46db294a3d7b03e606e50f593
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-03-23 16:43:45 -0500

    Use Mongoose population to cross-reference users with a comment and populate comments with referenced users' information

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -11,6 +11,7 @@ campsiteRouter.use(bodyParser.json());// display json format//// body-parser ext
 campsiteRouter.route('/')
 .get((req, res, next) => {
     Campsite.find() // This is find all the "documents" in a collection
+    .populate('comments.author')
     .then(campsites => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -46,6 +47,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId') //this is called a URL Parameter
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -82,6 +84,7 @@ campsiteRouter.route('/:campsiteId') //this is called a URL Parameter
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -99,6 +102,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -146,6 +150,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
